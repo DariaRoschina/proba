8e962acd3e83e190c385ab34f51de1b4c49131e2
DariaRoschina <darya.roshina@bk.ru>
2014-09-26 12:50:59 +0400

    first version of server and client

---
+++ Server.cpp
@@ -0,0 +1,50 @@
+#include <Ice/Ice.h>
+#include <Printer.h>
+//#include <Printerclass.h>
+
+using namespace std;
+using namespace Demo;
+class PrinterI : public Printer {
+public:
+    virtual string getString(const Ice::Current&);
+};
+//const string& s, const Ice::Current&
+string
+PrinterI::
+getString(const Ice::Current&)
+{
+//const string& s, const Ice::Current&
+string s="hello";
+return s;
+    //cout << s << endl;
+}
+int
+main(int argc, char* argv[])
+{
+    int status = 0;
+    Ice::CommunicatorPtr ic;
+    try {
+        ic = Ice::initialize(argc, argv);
+        Ice::ObjectAdapterPtr adapter =
+            ic->createObjectAdapterWithEndpoints("SimplePrinterAdapter", "default -p 10000");
+        Ice::ObjectPtr object = new PrinterI;
+        adapter->add(object, ic->stringToIdentity("SimplePrinter"));
+        adapter->activate();
+        ic->waitForShutdown();
+    } catch (const Ice::Exception& e) {
+        cerr << e << endl;
+        status = 1;
+    } catch (const char* msg) {
+        cerr << msg << endl;
+        status = 1;
+    }
+    if (ic) {
+        try {
+            ic->destroy();
+        } catch (const Ice::Exception& e) {
+            cerr << e << endl;
+            status = 1;
+        }
+    }
+    return status;
+}
