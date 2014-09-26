b9b6a635527bedf3c01248280ac47774a1e0a8c5
DariaRoschina <darya.roshina@bk.ru>
2014-09-26 12:59:09 +0400

    first version of  client

---
+++ client.cpp
@@ -0,0 +1,33 @@
+#include <Ice/Ice.h>
+#include <Printer.h>
+//#include <Printerclass.h>
+
+using namespace std;
+using namespace Demo;
+string s;
+int
+main(int argc, char* argv[])
+{
+    int status = 0;
+    Ice::CommunicatorPtr ic;
+    try {
+        ic = Ice::initialize(argc, argv);
+        Ice::ObjectPrx base = ic->stringToProxy("SimplePrinter:default -p 10000");
+        PrinterPrx printer = PrinterPrx::checkedCast(base);
+        if (!printer)
+            throw "Invalid proxy";
+
+       // printer->printString("!");
+s=printer->getString();
+cout << s;
+    } catch (const Ice::Exception& ex) {
+        cerr << ex << endl;
+        status = 1;
+    } catch (const char* msg) {
+        cerr << msg << endl;
+        status = 1;
+    }
+    if (ic)
+        ic->destroy();
+    return status;
+}
